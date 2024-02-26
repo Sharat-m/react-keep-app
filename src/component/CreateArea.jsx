f832a33d87edc989bb33b02bdd2b35fb1de6899c
sharat <sharat@farefirst.com>
2024-02-26 17:08:21 +0530

    Using dependencies in react

--- src/component/CreateArea.jsx
+++ src/component/CreateArea.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
+
 
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
-
+  
+  const [isExpanded , setExpanded]= useState(false)
   function handleChange(event) {
     const { name, value } = event.target;
 
@@ -25,25 +30,35 @@ function CreateArea(props) {
     });
     event.preventDefault();
   }
+  function expand() {
+    setExpanded(true)
+  }
 
   return (
     <div>
+     
       <form className="create-note">
-        <input
+      {isExpanded ? <input
           name="title"
           onChange={handleChange}
           value={note.title}
           placeholder="Title"
-        />
+        />: null}
+        
+        
         <textarea
+        onClick={expand}
           name="content"
           onChange={handleChange}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          rows={isExpanded ? 3 : 1}
         />
-        <button onClick={submitNote}>Add</button>
+        < Zoom in={isExpanded}>
+        <Fab onClick={submitNote}><AddIcon /></Fab>
+        </Zoom>
       </form>
+      
     </div>
   );
 }
